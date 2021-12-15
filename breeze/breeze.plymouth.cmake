4dfaff3f86c127bd1fa6c83f46d83c3722bf9784
Harald Sitter <sitter@kde.org>
2021-12-15 13:37:00 +0100

    shrinkety

--- breeze/breeze.plymouth.cmake
+++
@@ -1,8 +0,0 @@
-[Plymouth Theme]
-Name=Breeze
-Description=A Breeze Splash
-ModuleName=script
-
-[script]
-ImageDir=@Plymouth_THEMESDIR@/breeze
-ScriptFile=@Plymouth_THEMESDIR@/breeze/breeze.script
---
+++ breeze/neon-shrink.plymouth.cmake
@@ -0,0 +1,8 @@
+[Plymouth Theme]
+Name=Neon Shrink 1080
+Description=
+ModuleName=script
+
+[script]
+ImageDir=@Plymouth_THEMESDIR@/neon-shrink
+ScriptFile=@Plymouth_THEMESDIR@/neon-shrink/neon-shrink.script
